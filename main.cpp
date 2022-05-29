27889064b412d946d0e6d27aaf7b072caba2fc8f
beromes <nunesbernardo2000@gmail.com>
2022-05-29 20:45:28 -0300

    Criando fluxo principal

--- main.cpp
+++ main.cpp
@@ -2,13 +2,44 @@
 #include "place.h"
 #include "reservation.h"
 
+
+void printMainMenu() {
+    printf("\n");
+    printf("/=======================\\\n");
+    printf("| 1. Adicionar Espaço   |\n");
+    printf("| 2. Adicionar Reserva  |\n");
+    printf("| 3. Listar Reservas    |\n");
+    printf("| 4. Sair               |\n");
+    printf("\\=======================/\n");
+
+    printf("Opção selecionada: ");
+}
+
+
 int main() {
 
-    Place place = Place("Sala 213");
+    int menu = 0;
 
-    Reservation reservation = Reservation(place);
+    while(menu != 4) {
 
-    printf("Hello World\n");
+        printMainMenu();
+        scanf("%d", &menu);
+
+        switch (menu) {
+            case 1:
+                // To Do
+                break;
+            case 2:
+                // To Do          
+                break;
+            case 3:
+                // To Do
+                break;
+            default:
+                break;
+        }
+    
+    }
 
     return 0;
 }
